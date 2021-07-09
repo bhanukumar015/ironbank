c692b3a2cbab5e3d9881c6c0c20000c4163e1177
Amit Jaiswal <amit.jaiswal7026@gmail.com>
2021-07-09 13:36:33 +0530

    defined cardProgram dataStructure

--- src/main/groovy/hyperface/cms/domains/CreditCardProgram.groovy
+++ src/main/groovy/hyperface/cms/domains/CreditCardProgram.groovy
@@ -8,7 +8,9 @@ import javax.persistence.EnumType
 import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
+import java.time.ZonedDateTime
 
 @Entity
 class CreditCardProgram extends HyperfaceProgram {
@@ -33,10 +35,26 @@ class CreditCardProgram extends HyperfaceProgram {
     String cardLogoId
     String cardPlasticId
 
+    String startingCardNumber
+    String endingCardNumber
+    String lastUsedCardNumber
+
+    Boolean domesticUsage
+    Boolean internationalUsage
+    Boolean cardIssuing = true
+
+    ZonedDateTime dueDate
+    Integer gracePeriod //Days
+    Double minimumAmountDue
+
+    Double overlimitAuth //percentage
+    Double cashAdvanceLimit //percentage
+
     @ManyToOne
     CreditCardScheduleOfCharges scheduleOfCharges
 
     @ManyToOne
+    @JoinColumn(name="card_bin_id")
     CardBin cardBin
 
     @ManyToOne
