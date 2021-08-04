c2670cd25187c00b06a4f74f2a447b0c68e2be32
vamc-stash <rangavamsi5@gmail.com>
2021-08-04 09:59:52 +0530

    added x-request-id to log

--- src/main/groovy/hyperface/cms/service/SwitchProviders/Nium/Utility/NiumRestUtils.groovy
+++ src/main/groovy/hyperface/cms/service/SwitchProviders/Nium/Utility/NiumRestUtils.groovy
@@ -1,11 +1,13 @@
 package hyperface.cms.service.SwitchProviders.Nium.Utility
 
+import groovy.util.logging.Slf4j
 import hyperface.cms.config.SwitchProvidersConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 
 @Service
+@Slf4j
 class NiumRestUtils {
 
     @Autowired
@@ -25,6 +27,7 @@ class NiumRestUtils {
         headers.put('x-client-name', switchProvidersConfig.niumClientName)
         headers.put(HttpHeaders.CONTENT_TYPE, 'application/json')
         headers.put('x-request-id', UUID.randomUUID().toString())
+        log.info("Request to Nium with request id: ${headers.get("x-request-id")} is passed")
         return headers
     }
 }
